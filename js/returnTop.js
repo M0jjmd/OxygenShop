f9cfc1a603ab820b5b3f4ba2b8c9d1d4a5563512
Mojak <moamine517@gmail.com>
2024-06-14 18:09:28 +0200

    Finished scroll to top, now works with scroll progress

---
+++ js/returnTop.js
@@ -0,0 +1,33 @@
+// addEventListener("DOMContentLoaded", (event) => {
+//     console.log("test")
+//     window.onscroll = () => {
+//         toggleScrollTopBtn();
+//     };
+// });
+
+// window.onscroll = () => {
+//     toggleScrollTopBtn();
+// };
+
+// function toggleScrollTopBtn() {
+//     let scrollTopBtn = document.getElementById("returnTop");
+//     let winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+//     let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+//     let scrolled = (winScroll / height) * 100;
+//     if (scrolled > 30) {
+//         console.log("so entra")
+//         scrollTopBtn.style.display = "block";
+//     } else {
+//         scrollTopBtn.style.display = "none";
+//     }
+// }
+
+// document.getElementById("returnTop").addEventListener("click", function () {
+//     console.log("test button")
+//     setTimeout(function () {
+//         window.scrollTo({
+//             top: 0,
+//             behavior: "smooth"
+//         });
+//     }, 2000);
+// });
