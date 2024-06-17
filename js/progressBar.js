0a2385dc72f2dd9cf70be168e6963002a9cc38ef
Mojak <moamine517@gmail.com>
2024-06-17 11:00:27 +0200

    Changed window.onscroll to addEventListener and split return to top and progress bar code

--- js/progressBar.js
+++ js/progressBar.js
@@ -1,10 +1,6 @@
-// window.onscroll = function() {
-//     updateProgressBar();
-// };
-window.onscroll = () => {
+window.addEventListener('scroll', () => {
     updateProgressBar();
-    toggleScrollTopBtn();
-};
+});
 
 function updateProgressBar() {
     let winScroll = document.body.scrollTop || document.documentElement.scrollTop;
@@ -12,26 +8,3 @@ function updateProgressBar() {
     let scrolled = (winScroll / height) * 100;
     document.getElementById("progress-bar").style.width = scrolled + "%";
 }
-
-function toggleScrollTopBtn() {
-    let scrollTopBtn = document.getElementById("returnTop");
-    let winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-    let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-    let scrolled = (winScroll / height) * 100;
-    if (scrolled > 30) {
-        console.log("so entra")
-        scrollTopBtn.style.display = "block";
-    } else {
-        scrollTopBtn.style.display = "none";
-    }
-}
-
-document.getElementById("returnTop").addEventListener("click", function () {
-    console.log("test button")
-    setTimeout(function () {
-        window.scrollTo({
-            top: 0,
-            behavior: "smooth"
-        });
-    }, 2000);
-});
--- js/returnTop.js
+++ js/returnTop.js
@@ -1,33 +1,26 @@
-// addEventListener("DOMContentLoaded", (event) => {
-//     console.log("test")
-//     window.onscroll = () => {
-//         toggleScrollTopBtn();
-//     };
-// });
+window.addEventListener('scroll', () => {
+    toggleScrollTopBtn();
+});
 
-// window.onscroll = () => {
-//     toggleScrollTopBtn();
-// };
+function toggleScrollTopBtn() {
+    let scrollTopBtn = document.getElementById("returnTop");
+    let winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    let scrolled = (winScroll / height) * 100;
+    if (scrolled > 30) {
+        console.log("so entra")
+        scrollTopBtn.style.display = "block";
+    } else {
+        scrollTopBtn.style.display = "none";
+    }
+}
 
-// function toggleScrollTopBtn() {
-//     let scrollTopBtn = document.getElementById("returnTop");
-//     let winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-//     let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-//     let scrolled = (winScroll / height) * 100;
-//     if (scrolled > 30) {
-//         console.log("so entra")
-//         scrollTopBtn.style.display = "block";
-//     } else {
-//         scrollTopBtn.style.display = "none";
-//     }
-// }
-
-// document.getElementById("returnTop").addEventListener("click", function () {
-//     console.log("test button")
-//     setTimeout(function () {
-//         window.scrollTo({
-//             top: 0,
-//             behavior: "smooth"
-//         });
-//     }, 2000);
-// });
+document.getElementById("returnTop").addEventListener("click", function () {
+    console.log("test button")
+    setTimeout(function () {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        });
+    }, 2000);
+});
